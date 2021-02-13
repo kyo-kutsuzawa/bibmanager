e0d8ad8de936aa4c90526d19f8004b12b0bd326f
kyo-kutsuzawa <k.kutsuzawa.430@gmail.com>
2021-02-13 16:50:01 +0900

    Change quotation marks

--- main.js
+++ main.js
@@ -1,20 +1,20 @@
-const electron = require('electron');
+const electron = require("electron");
 const app = electron.app;
 const BrowserWindow = electron.BrowserWindow;
 
 let mainWindow = null;
 
 
-app.on('ready', () => {
+app.on("ready", () => {
     // Create the main window (here you can also set the window size, whether the Kiosk mode enables, etc.)
-    mainWindow = new BrowserWindow({width: 400, height: 300});
+    mainWindow = new BrowserWindow({width: 640, height: 480});
     // Specify html file to show in Electron by absolute path (note that relative path does not work)
-    mainWindow.loadURL('file://' + __dirname + '/index.html');
+    mainWindow.loadURL("file://" + __dirname + "/index.html");
 
     // Open a dev tool of Chromium
     mainWindow.webContents.openDevTools();
 
-    mainWindow.on('closed', function() {
-    mainWindow = null;
-  });
+    mainWindow.on("closed", function() {
+        mainWindow = null;
+    });
 });
